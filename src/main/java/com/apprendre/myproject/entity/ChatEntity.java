1763e67efb63ba9aa3f2e4491d96e2e2193249d8
Jean-Jacques-Augustin <rjja@outlook.fr>
2023-11-08 14:55:01 +0300

    update

---
+++ src/main/java/com/apprendre/myproject/entity/ChatEntity.java
@@ -0,0 +1,28 @@
+package com.apprendre.myproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chat")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
